0dbce54b94363c8ebb3c24095f28fc341e22a0e4
Dipta Dhar <65239718+DiptaDhar2020@users.noreply.github.com>
2024-09-16 21:02:01 +0600

    Update HeartSoundClassifier.py
    Extra things

--- HeartSoundClassifier.py
+++ HeartSoundClassifier.py
@@ -3,6 +3,19 @@ from keras.models import load_model
 import librosa
 import numpy as np
 
+# Disclaimer Text
+disclaimer = """
+**Disclaimer: This heart sound classification app is a tool designed to assist in the preliminary assessment of heart sounds. It is not a substitute for a professional medical diagnosis. The app uses an artificial neural network (ANN) model with approximately 95% accuracy, but its accuracy may vary depending on the quality of the heart sound recording and other factors. If you have concerns about your heart health, please consult a healthcare professional for a proper evaluation and diagnosis.
+"""
+resources = """
+Additional Resources:
+  - Bangladesh Cardiac Society: https://banglacardio.org/
+  - National Heart Foundation of Bangladesh: https://www.nhf.org.bd/
+"""
+
+# Copyright Text
+copyright = "©diptadhar2024"
+
 # Load the model
 model = load_model("bin_classification.hdf5")
 
@@ -16,8 +29,10 @@ def classify_heartbeat(filename):
     return predicted_label[0][0] * 100
 
 # Streamlit app
-st.title("Heart Sound Classification App")
+# Set wide mode for the app
+st.set_page_config(layout="wide")
 
+st.markdown("<h1 style='text-align: center; margin-top:-15px;'>Heart Sound Classification App</h1>", unsafe_allow_html=True)
 uploaded_file = st.file_uploader("Choose a heart sound wav file", type="wav")
 
 if uploaded_file is not None:
@@ -30,3 +45,8 @@ if uploaded_file is not None:
         st.success("Normal Heartbeat.")
     else:
         st.warning("Abnormal Heartbeat.")
+
+# Display Disclaimer and Copyright
+st.markdown(f"<center style='margin-top: 30px; text-align: justify; text-align-last: center; font-size: 18px;'>{disclaimer}</center>", unsafe_allow_html=True)
+st.markdown(resources, unsafe_allow_html=True)
+st.markdown(f"<center style='margin-top: 20px;'>{copyright}</center>", unsafe_allow_html=True)
